fdc7f88c2d8324b5ee4496ef7525b1e2638c0243
Ashok <er.ashok23@gmail.com>
2022-05-31 14:07:59 +0530

    role seeder added user_type_id

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -24,22 +24,134 @@ class RoleSeeder extends Seeder
        
          
         /*------------Default Role-----------------------------------*/
-        $role1 = Role::create(['id' => '1','name' => 'Admin','se_name' => 'Super Admin', 'guard_name' => 'api','is_default'=>'0', 'user_type_id' => 1]);
-        $role2 = Role::create(['id' => '2','name' => 'Company','se_name' => 'Company', 'guard_name' => 'api','is_default'=>'0']);
-        $role3 = Role::create(['id' => '3','name' => 'Employee','se_name' => 'Employee', 'guard_name' => 'api','is_default'=>'1']);
-        $role4 = Role::create(['id' => '4','name' => 'Hospital','se_name' => 'Hospital', 'guard_name' => 'api','is_default'=>'1']);
-        $role5 = Role::create(['id' => '5','name' => 'Nuser','se_name' => 'Nuser', 'guard_name' => 'api','is_default'=>'1']);
-        $role6 = Role::create(['id' => '6','name' => 'Patient','se_name' => 'Patient', 'guard_name' => 'api','is_default'=>'1']);
-        $role7 = Role::create(['id' => '7','name' => 'careTaker','se_name' => 'careTaker', 'guard_name' => 'api','is_default'=>'1']);
-        $role8 = Role::create(['id' => '8','name' => 'FamilyMember','se_name' => 'FamilyMember', 'guard_name' => 'api','is_default'=>'1']);
-        $role9 = Role::create(['id' => '9','name' => 'ContactPerson','se_name' => 'ContactPerson', 'guard_name' => 'api','is_default'=>'1']);
-        $role10 = Role::create(['id' => '10','name' => 'careTakerFamily','se_name' => 'careTakerFamily', 'guard_name' => 'api','is_default'=>'1']);
-        $role11 = Role::create(['id' => '11','name' => 'Branch','se_name' => 'Branch', 'guard_name' => 'api','is_default'=>'1']);
-        $role12 = Role::create(['id' => '12','name' => 'Guardian','se_name' => 'Guardian', 'guard_name' => 'api','is_default'=>'1']);
-        $role13 = Role::create(['id' => '13','name' => 'Presented','se_name' => 'Presented', 'guard_name' => 'api','is_default'=>'1']);
-        $role14 = Role::create(['id' => '14','name' => 'Participated','se_name' => 'Participated', 'guard_name' => 'api','is_default'=>'1']);
-        $role15 = Role::create(['id' => '15','name' => 'Other','se_name' => 'Other', 'guard_name' => 'api','is_default'=>'1']);
-        $role16 = Role::create(['id' => '16','name' => 'Admin Employee','se_name' => 'Admin Employee', 'guard_name' => 'api','is_default'=>'0']);
+        $role1 = Role::create([
+            'id' => '1',
+            'name' => 'Admin',
+            'se_name' => 'Super Admin', 
+            'guard_name' => 'api',
+            'is_default'=>'0', 
+            'user_type_id' => 1,
+        ]);
+        $role2 = Role::create([
+            'id' => '2',
+            'name' => 'Company',
+            'se_name' => 'Company', 
+            'guard_name' => 'api',
+            'is_default'=>'0', 
+            'user_type_id' => 2
+        ]);
+        $role3 = Role::create([
+            'id' => '3',
+            'name' => 'Employee',
+            'se_name' => 'Employee', 
+            'guard_name' => 'api',
+            'is_default'=>'1', 
+            'user_type_id' => 3
+        ]);
+        $role4 = Role::create([
+            'id' => '4',
+            'name' => 'Hospital',
+            'se_name' => 'Hospital', 
+            'guard_name' => 'api',
+            'is_default'=>'1', 
+            'user_type_id' => 4
+        ]);
+        $role5 = Role::create([
+            'id' => '5',
+            'name' => 'Nuser',
+            'se_name' => 'Nuser', 
+            'guard_name' => 'api',
+            'is_default'=>'1', 
+            'user_type_id' => 5
+        ]);
+        $role6 = Role::create([
+            'id' => '6',
+            'name' => 'Patient',
+            'se_name' => 'Patient', 
+            'guard_name' => 'api',
+            'is_default'=>'1', 
+            'user_type_id' => 6
+        ]);
+        $role7 = Role::create([
+            'id' => '7',
+            'name' => 'careTaker',
+            'se_name' => 'careTaker', 
+            'guard_name' => 'api',
+            'is_default'=>'1', 
+            'user_type_id' => 7
+        ]);
+        $role8 = Role::create([
+            'id' => '8',
+            'name' => 'FamilyMember',
+            'se_name' => 'FamilyMember', 
+            'guard_name' => 'api',
+            'is_default'=>'1', 
+            'user_type_id' => 8
+        ]);
+        $role9 = Role::create([
+            'id' => '9',
+            'name' => 'ContactPerson',
+            'se_name' => 'ContactPerson', 
+            'guard_name' => 'api',
+            'is_default'=>'1', 
+            'user_type_id' => 9
+        ]);
+        $role10 = Role::create([
+            'id' => '10',
+            'name' => 'careTakerFamily',
+            'se_name' => 'careTakerFamily', 
+            'guard_name' => 'api',
+            'is_default'=>'1', 
+            'user_type_id' => 10
+        ]);
+        $role11 = Role::create([
+            'id' => '11',
+            'name' => 'Branch',
+            'se_name' => 'Branch', 
+            'guard_name' => 'api',
+            'is_default'=>'1', 
+            'user_type_id' => 11
+        ]);
+        $role12 = Role::create([
+            'id' => '12',
+            'name' => 'Guardian',
+            'se_name' => 'Guardian', 
+            'guard_name' => 'api',
+            'is_default'=>'1', 
+            'user_type_id' => 12
+        ]);
+        $role13 = Role::create([
+            'id' => '13',
+            'name' => 'Presented',
+            'se_name' => 'Presented', 
+            'guard_name' => 'api',
+            'is_default'=>'1', 
+            'user_type_id' => 13
+        ]);
+        $role14 = Role::create([
+            'id' => '14',
+            'name' => 'Participated',
+            'se_name' => 'Participated', 
+            'guard_name' => 'api',
+            'is_default'=>'1', 
+            'user_type_id' => 14
+        ]);
+        $role15 = Role::create([
+            'id' => '15',
+            'name' => 'Other',
+            'se_name' => 'Other', 
+            'guard_name' => 'api',
+            'is_default'=>'1', 
+            'user_type_id' => 15
+        ]);
+        $role16 = Role::create([
+            'id' => '16',
+            'name' => 'Admin Employee',
+            'se_name' => 'Admin Employee', 
+            'guard_name' => 'api',
+            'is_default'=>'0', 
+            'user_type_id' => 16
+        ]);
 
     }
 }
