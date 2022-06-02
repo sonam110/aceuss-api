7a0e20b8330737fe9211b182f34b91d03342faaa
Ashok <er.ashok23@gmail.com>
2022-06-02 17:46:40 +0530

    messages changes

--- app/Http/Controllers/Api/V1/Common/MessagingController.php
+++ app/Http/Controllers/Api/V1/Common/MessagingController.php
@@ -139,7 +139,7 @@ class MessagingController extends Controller
                 ->where(function($q) use ($user_id) {
                     $q->where('sender_id', auth()->id())
                         ->orWhere('receiver_id', $user_id);
-                });
+                })
                 ->get();
             }
             
