274397fb8fab7b2e70f6aab2bae6ac70de87e659
khushboo <khushboo.kumari@newrise.in>
2022-06-03 16:25:27 +0530

    ovhours

--- app/Http/Controllers/Api/V1/User/OVHourController.php
+++ app/Http/Controllers/Api/V1/User/OVHourController.php
@@ -3,30 +3,30 @@
 namespace App\Http\Controllers\API\V1\User;
 
 use App\Http\Controllers\Controller;
-use App\Models\OvHour;
+use App\Models\OVHour;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Str;
 use DB;
 
-class OvHourController extends Controller
+class OVHourController extends Controller
 {
     // public function __construct()
     // {
 
-    //     $this->middleware('permission:OvHour-browse',['except' => ['show']]);
-    //     $this->middleware('permission:OvHour-add', ['only' => ['store']]);
-    //     $this->middleware('permission:OvHour-edit', ['only' => ['update']]);
-    //     $this->middleware('permission:OvHour-read', ['only' => ['show']]);
-    //     $this->middleware('permission:OvHour-delete', ['only' => ['destroy']]);
+    //     $this->middleware('permission:OVHour-browse',['except' => ['show']]);
+    //     $this->middleware('permission:OVHour-add', ['only' => ['store']]);
+    //     $this->middleware('permission:OVHour-edit', ['only' => ['update']]);
+    //     $this->middleware('permission:OVHour-read', ['only' => ['show']]);
+    //     $this->middleware('permission:OVHour-delete', ['only' => ['destroy']]);
         
     // }
 
-    public function OvHours(Request $request)
+    public function OVHours(Request $request)
     {
         try {
 
-            $query = OvHour::orderBy('id', 'DESC');
+            $query = OVHour::orderBy('id', 'DESC');
             if(!empty($request->perPage))
             {
                 $perPage = $request->perPage;
@@ -41,13 +41,13 @@ class OvHourController extends Controller
                     'per_page' => $perPage,
                     'last_page' => ceil($total / $perPage)
                 ];
-                return prepareResult(true,"OvHour list",$pagination,config('httpcodes.success'));
+                return prepareResult(true,"OVHour list",$pagination,config('httpcodes.success'));
             }
             else
             {
                 $query = $query->get();
             }
-            return prepareResult(true,"OvHour list",$query,config('httpcodes.success'));
+            return prepareResult(true,"OVHour list",$query,config('httpcodes.success'));
         }
         catch(Exception $exception) {
             return prepareResult(false, $exception->getMessage(),[], config('httpcodes.internal_server_error'));
@@ -64,7 +64,7 @@ class OvHourController extends Controller
 
     public function store(Request $request)
     {
-        if(empty($request->date))
+        if(empty($request->dates))
         {
             $validation = \Validator::make($request->all(), [
                 'start_time'      => 'required',
@@ -91,61 +91,66 @@ class OvHourController extends Controller
         try {
             if($request->is_range == 1)
             {
-                $start_date = $request->date[0];
-                $end_date = $request->date[1];
-                $diffrece = dateDifference($start_date, $end_date) + 1;
+                $start_date = $request->dates[0];
+                $end_date = $request->dates[1];
+                $is_repeat = 1;
+                $everyWeek = $request->every_week;
+                $week_days = $request->week_days;
+                   
+                $from = \Carbon\Carbon::parse($start_date);
+                $to =   (!empty($end_date)) ? \Carbon\Carbon::parse($end_date) : \Carbon\Carbon::parse($start_date);
+                $start_from = $from->format('Y-m-d');
+                $end_to = $to->format('Y-m-d');
+
                 $dates = [];
                 $ovhour_ids = [];
 
-                for($i = $diffrece; $i>=1; $i--)
+                if($request->is_repeat == 1)
                 {
-                    $dates[] = date("Y-m-d", strtotime('-'.($i-1).' days', strtotime($end_date)));
-                }  
-                foreach ($dates as $key => $date) 
-                {
-                    if($request->is_repeat == 1)
-                    {
-                        $everyWeek = $request->every_week;
-                        $week_days = $request->week_days;
-                        foreach ($week_days as $key => $value) {
-                            if(date('w',strtotime($date)) == $value)
-                            {
-                                $ovHour = new OvHour;
-                                $ovHour->date = $date;
-                                $ovHour->start_time = $request->start_time;
-                                $ovHour->end_time = $request->end_time;
-                                $ovHour->entry_mode = $request->entry_mode;
-                                $ovHour->save();
-
-                                $ovhour_ids[] = $ovHour->id;
+                    for($w = $from; $w->lte($to); $w->addWeeks($everyWeek)) {
+                        $date = \Carbon\Carbon::parse($w);
+                        $startWeek = $w->startOfWeek()->format('Y-m-d');
+                        $weekNumber = $date->weekNumberInMonth;
+                        $start = \Carbon\Carbon::createFromFormat("Y-m-d", $startWeek);
+                        $end = $start->copy()->endOfWeek()->format('Y-m-d');
+                        for($p = $start; $p->lte($end); $p->addDays()) {
+                            if(strtotime($start_from) <= strtotime($p) && strtotime($end_to) >= strtotime($p) ) {
+                                if(in_array($p->dayOfWeek, $week_days)){
+                                    array_push($dates, $p->copy()->format('Y-m-d'));
+                                }
                             }
                         }
                     }
-                    else
-                    {
-                        $ovHour = new OvHour;
-                        $ovHour->date = $date;
-                        $ovHour->start_time = $request->start_time;
-                        $ovHour->end_time = $request->end_time;
-                        $ovHour->entry_mode = $request->entry_mode;
-                        $ovHour->save();
-                        $ovhour_ids[] = $ovHour->id;
+                }
+                else
+                {
+                    $date1 = strtotime($start_date);
+                    $date2 = strtotime($end_date);
+                    for ($currentDate=$date1; $currentDate<=$date2; $currentDate += (86400)) 
+                    {                                   
+                        $dates[] = date('Y-m-d', $currentDate);
                     }
-                }              
+                }         
             }
             else
             {
-                $date = $request->date;
-                $ovHour = new OvHour;
+                $dates = $request->dates;
+            }
+
+            foreach ($dates as $key => $date) 
+            {
+                $ovHour = new OVHour;
                 $ovHour->date = $date;
                 $ovHour->start_time = $request->start_time;
                 $ovHour->end_time = $request->end_time;
                 $ovHour->entry_mode = $request->entry_mode;
                 $ovHour->save();
-            }
-            
+                $ovhour_ids[] = $ovHour->id;
+            } 
+
+            $data = OVHour::whereIn('id',$ovhour_ids)->get();
             DB::commit();
-            return prepareResult(true,getLangByLabelGroups('OvHour','message_create') ,$ovHour, config('httpcodes.success'));
+            return prepareResult(true,getLangByLabelGroups('OVHour','message_create') ,$data, config('httpcodes.success'));
         } catch (\Throwable $exception) {
             \Log::error($exception);
             DB::rollback();
@@ -156,18 +161,18 @@ class OvHourController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  \App\OvHour  $ovHour
+     * @param  \App\OVHour  $ovHour
      * @return \Illuminate\Http\Response
      */
     public function show($id)
     {
         try 
         {
-            $checkId= OvHour::find($id);
+            $checkId= OVHour::find($id);
             if (!is_object($checkId)) {
-                return prepareResult(false,getLangByLabelGroups('OvHour','message_id_not_found'), [],config('httpcodes.not_found'));
+                return prepareResult(false,getLangByLabelGroups('OVHour','message_id_not_found'), [],config('httpcodes.not_found'));
             }
-             return prepareResult(true,'View OvHour' ,$checkId, config('httpcodes.success'));
+             return prepareResult(true,'View OVHour' ,$checkId, config('httpcodes.success'));
         } catch (\Throwable $exception) {
             \Log::error($exception);
             return prepareResult(false, $exception->getMessage(),[], config('httpcodes.internal_server_error'));
@@ -178,7 +183,7 @@ class OvHourController extends Controller
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\OvHour  $ovHour
+     * @param  \App\OVHour  $ovHour
      * @return \Illuminate\Http\Response
      */
     
@@ -209,14 +214,14 @@ class OvHourController extends Controller
 
         DB::beginTransaction();
         try {
-            $ovHour = OvHour::find($id);
+            $ovHour = OVHour::find($id);
             $ovHour->date = $request->date;
             $ovHour->start_time = $request->start_time;
             $ovHour->end_time = $request->end_time;
             $ovHour->entry_mode = $request->entry_mode;
             $ovHour->save();
             DB::commit();
-            return prepareResult(true,getLangByLabelGroups('OvHour','message_create') ,$ovHour, config('httpcodes.success'));
+            return prepareResult(true,getLangByLabelGroups('OVHour','message_create') ,$ovHour, config('httpcodes.success'));
         } catch (\Throwable $exception) {
             \Log::error($exception);
             DB::rollback();
@@ -227,7 +232,7 @@ class OvHourController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param \App\OvHour $ovHour
+     * @param \App\OVHour $ovHour
      * @return \Illuminate\Http\Response
      * @throws \Exception
      */
@@ -236,14 +241,14 @@ class OvHourController extends Controller
     {
         try 
         {
-            $checkId= OvHour::find($id);
+            $checkId= OVHour::find($id);
             if (!is_object($checkId)) {
-                return prepareResult(false,getLangByLabelGroups('OvHour','message_id_not_found'), [],config('httpcodes.not_found'));
+                return prepareResult(false,getLangByLabelGroups('OVHour','message_id_not_found'), [],config('httpcodes.not_found'));
             }
             if(auth()->user()->user_type_id=='1')
             {
-                OvHour::where('id',$id)->delete();
-                return prepareResult(true,getLangByLabelGroups('OvHour','message_delete') ,[], config('httpcodes.success'));
+                OVHour::where('id',$id)->delete();
+                return prepareResult(true,getLangByLabelGroups('OVHour','message_delete') ,[], config('httpcodes.success'));
             }
            return prepareResult(false, 'Record Not Found', [],config('httpcodes.not_found'));
             
