9d8e13c6a381ba2436c387298f44363151da4f77
Ashok <er.ashok23@gmail.com>
2022-06-11 17:02:26 +0530

    function correct

--- app/Http/Controllers/Api/V1/Common/WebSocketController.php
+++ app/Http/Controllers/Api/V1/Common/WebSocketController.php
@@ -138,7 +138,7 @@ class WebSocketController implements MessageComponentInterface {
                         if ($userToken['user_id'] != $data->logged_in_user_id ) {
                             $conn->send(json_encode('user not matched'));
                         } else  {
-                            $getmessages = $this->getmessages($data->logged_in_user_id,$data->other_user_id,$data->from_date,$data->end_date, $data->per_page);
+                            $getmessages = $this->getmessages($data->logged_in_user_id,$data->other_user_id,$data->from_date,$data->end_date, $data->per_page, $data->page);
                             $from_date = (!empty($data->from_date)) ? $data->from_date : (new Carbon)->subDays(7)->startOfDay()->toDateString();
                             $end_date = (!empty($data->end_date)) ? $data->end_date : (new Carbon)->now()->endOfDay()->toDateString();
                             $returnData = [
@@ -148,6 +148,7 @@ class WebSocketController implements MessageComponentInterface {
                                 'from_date' => $from_date,
                                 'end_date'  => $end_date
                                 'per_page'  => $per_page
+                                'page'  => $page
                             ];
                             $conn->send(json_encode($returnData));
                         }
@@ -332,7 +333,7 @@ class WebSocketController implements MessageComponentInterface {
         return $query;
     }
 
-    private function getmessages($logged_in_user_id, $other_user_id, $from_date, $end_date, $perPage)
+    private function getmessages($logged_in_user_id, $other_user_id, $from_date, $end_date, $perPage, $page)
     {
         $query = Message::with('sender:id,name,gender,user_type_id,avatar', 'receiver:id,name,gender,user_type_id,avatar', 'sender.UserType:id,name', 'receiver.UserType:id,name')
             ->whereIn('sender_id', [$logged_in_user_id, $other_user_id])
@@ -343,10 +344,10 @@ class WebSocketController implements MessageComponentInterface {
             
         }
 
-        if(!empty($request->perPage))
+        if(!empty($perPage))
         {
-            $perPage = $request->perPage;
-            $page = $request->input('page', 1);
+            $perPage = $perPage;
+            $page = !empty($page) ? $page : 1;
             $total = $query->count();
             $result = $query->offset(($page - 1) * $perPage)->limit($perPage)->orderBy('id', 'DESC')->get();
 
