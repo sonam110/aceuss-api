23df2735c4b98d8cfa007a1b814d0811bca10b32
Ashok Sahu <ashok@nrt.co.in>
2022-10-04 12:23:50 +0530

    column added

--- database/migrations/2022_06_02_101523_create_schedules_table.php
+++ database/migrations/2022_06_02_101523_create_schedules_table.php
@@ -79,6 +79,15 @@ class CreateSchedulesTable extends Migration
             $table->string('ob_type')->nullable();
             $table->string('ob_start_time')->nullable();
             $table->string('ob_end_time')->nullable();
+
+            $table->string('ob_red_end_time')->nullable();
+            $table->string('ob_end_time')->nullable();
+            $table->string('ob_weekend_start_time')->nullable();
+            $table->string('ob_weekend_end_time')->nullable();
+            $table->string('ob_weekday_start_time')->nullable();
+            $table->string('ob_weekday_end_time')->nullable();
+
+
             $table->boolean('approved_by_company')->default(0)->nullable();
             $table->string('company_sessionId')->nullable();
             $table->boolean('verified_by_employee')->default(0)->nullable();
