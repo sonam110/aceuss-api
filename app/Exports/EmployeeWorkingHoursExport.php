8ef0fceceb6161adb804cb1bbf285f981107d5e2
Ashok Sahu <ashok@nrt.co.in>
2022-12-08 12:33:17 +0530

    error fixed

--- app/Exports/EmployeeWorkingHoursExport.php
+++ app/Exports/EmployeeWorkingHoursExport.php
@@ -37,22 +37,32 @@ class EmployeeWorkingHoursExport implements FromCollection, WithHeadings
     public function collection()
     {
 
-    	$schedules  = Schedule::whereBetween('shift_date',$this->dates)->where('user_id',$this->user_id)->get();
+    	$schedules  = Schedule::whereBetween('shift_date',$this->dates)
+            ->where('user_id', $this->user_id)
+            ->where('is_active',1)
+            ->orderBy('shift_date', 'ASC')
+            ->get();
     	return $schedules->map(function ($data, $key) {
-    		$scheduled_work_duration = Schedule::where('shift_date',$data->shift_date)->where('user_id',$data->user_id)->sum('scheduled_work_duration');
-    		$extra_work_duration = Schedule::where('shift_date',$data->shift_date)->where('user_id',$data->user_id)->sum('extra_work_duration');
-    		$emergency_work_duration = Schedule::where('shift_date',$data->shift_date)->where('user_id',$data->user_id)->sum('emergency_work_duration');
-    		$ob_work_duration = Schedule::where('shift_date',$data->shift_date)->where('user_id',$data->user_id)->sum('ob_work_duration');
-    		$vacation_duration = Schedule::where('shift_date',$data->shift_date)->where('user_id',$data->user_id)->sum('vacation_duration');
-    		$total_hours = $scheduled_work_duration + $extra_work_duration + $emergency_work_duration + $ob_work_duration + $vacation_duration;
+
+            $scheduleData = Schedule::select(\DB::raw("SUM(scheduled_work_duration) as scheduled_work_duration"),
+                \DB::raw("SUM(extra_work_duration) as extra_work_duration"),
+                \DB::raw("SUM(emergency_work_duration) as emergency_work_duration"),
+                \DB::raw("SUM(ob_work_duration) as ob_work_duration"),
+                \DB::raw("SUM(vacation_duration) as vacation_duration")
+                )
+                ->where('shift_date',$data->shift_date)
+                ->where('user_id',$data->user_id)
+                ->where('is_active',1)
+                ->first();
+    		$total_hours = $scheduleData->scheduled_work_duration + $scheduleData->extra_work_duration + $scheduleData->emergency_work_duration + $scheduleData->ob_work_duration + $scheduleData->vacation_duration;
             return [
             	'Date' => $data->shift_date,
                 'Employee Name' => aceussDecrypt($data->user->name),
-	            'scheduled work duration' => minToHours($scheduled_work_duration),
-	            'extra work duration' => minToHours($extra_work_duration),
-	            'ob work duration' => minToHours($ob_work_duration),
-	            'emergency work duration' => minToHours($emergency_work_duration),
-	            'vacation duration' => minToHours($vacation_duration),
+	            'scheduled work duration' => minToHours($scheduleData->scheduled_work_duration),
+	            'extra work duration' => minToHours($scheduleData->extra_work_duration),
+	            'ob work duration' => minToHours($scheduleData->ob_work_duration),
+	            'emergency work duration' => minToHours($scheduleData->emergency_work_duration),
+	            'vacation duration' => minToHours($scheduleData->vacation_duration),
 	            'Total Hour' => minToHours($total_hours)
             ];
     	});
